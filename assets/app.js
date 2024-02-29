278366968e9837eeaeff434d9a70f2dd9e51e46f
Julian Llanten <hugojulians@gmail.com>
2024-02-28 20:02:25 -0500

    Add: agregar audio y controles

--- assets/app.js
+++ assets/app.js
@@ -1,23 +1,27 @@
-const $button = document.querySelector('button')
+const $button = document.querySelector("button");
 
-$button.addEventListener('click', async () => {
+$button.addEventListener("click", async () => {
   const media = await navigator.mediaDevices.getDisplayMedia({
-    video: { frameRate: { ideal: 30 } }
-  })
+    video: { frameRate: { ideal: 30 } },
+    audio: true,
+    controls: true,
+    syste,
+  });
   const mediarecorder = new MediaRecorder(media, {
-    mimeType: 'video/webm;codecs=vp8,opus'
-  })
-  mediarecorder.start()
+    mimeType: "video/webm;codecs=vp8,opus",
+  });
+  mediarecorder.start();
 
-  const [video] = media.getVideoTracks()
+  const [video] = media.getVideoTracks();
+
   video.addEventListener("ended", () => {
-    mediarecorder.stop()
-  })
+    mediarecorder.stop();
+  });
 
   mediarecorder.addEventListener("dataavailable", (e) => {
-    const link = document.createElement("a")
-    link.href = URL.createObjectURL(e.data)
-    link.download = "captura.webm"
-    link.click()
-  })
-})
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(e.data);
+    link.download = "captura.webm";
+    link.click();
+  });
+});
